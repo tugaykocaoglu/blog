46ea82a1fffbf2f70bad11ce5ed5b09da0bfeee6
tugaykocaoglu <tugay.kocaoglu@gmail.com>
2022-05-05 22:01:10 +0300

    Action creator in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //Lodash library
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 //ASYNC ACTION CREATOR WITH REDUX THUNK
 export const fetchPosts = () => async (dispatch) => {
@@ -6,3 +15,17 @@ export const fetchPosts = () => async (dispatch) => {
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
